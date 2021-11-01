e700b1213a1129469008522158d4eb4716299dae
Егор Кузнецов <e.kuznetsov1@g.nsu.ru>
2021-11-02 00:56:18 +0700

    first fixing

---
+++ Linear-Workflow/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "workflow.h"
+
+using namespace wkfw;
+
+int main(){
+    const std::string i = "in.txt";
+    const std::string o = "out.txt";
+    Workflow wflow(i, o);
+    wflow.run();
+    if(wflow){
+        std::cout<< "Workes" << std::endl;
+    }
+    else{
+        std::cout << "No workes" << std::endl;
+    }
+    return 0;
+}
